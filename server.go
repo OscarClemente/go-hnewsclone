011ad5452338784fb2955f08a829c7916d5b5d1f
OscarClemente <oscar.clemente@gmail.com>
2021-04-17 19:26:33 +0200

    Added working go backend for hackernews simple clone

---
+++ server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/OscarClemente/go-hnewsclone/graph"
+	"github.com/OscarClemente/go-hnewsclone/graph/generated"
+	"github.com/OscarClemente/go-hnewsclone/internal/auth"
+	database "github.com/OscarClemente/go-hnewsclone/internal/pkg/db/mysql"
+	"github.com/go-chi/chi"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(auth.Middleware())
+
+	database.InitDB()
+	database.Migrate()
+	server := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", server)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
